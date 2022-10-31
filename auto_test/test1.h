5a925bb6ca5c7cb370def0641ec10a0d77a39344
insppp <freefish_zhou@foxmail.com>
2022-11-01 00:10:37 +0800

    optimize and new autotest

---
+++ auto_test/test1.h
@@ -0,0 +1,11 @@
+//
+// Created by airvoltex on 22-10-31.
+//
+
+#ifndef COMMAND_LINE_EMB_COMMAND_LINE_TEST_H
+#define COMMAND_LINE_EMB_COMMAND_LINE_TEST_H
+
+#include "sys_app/command_line.h"
+int get_free_space(command_line_input_buffer_t * buffer);
+
+#endif //COMMAND_LINE_EMB_COMMAND_LINE_TEST_H
